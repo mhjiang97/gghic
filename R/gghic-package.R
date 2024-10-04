2372adba82d62aee6db12145f13e842c330737a7
Minghao Jiang | Office <jiangminghao1001@163.com>
2024-10-04 16:55:29 +0800

    Refactor gghic-package.R and gghic-package.Rd files

--- R/gghic-package.R
+++ R/gghic-package.R
@@ -2,5 +2,25 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom AnnotationDbi as.list loadDb saveDb
+#' @importFrom biovizBase getBioColor
+#' @importFrom dplyr bind_rows case_when distinct filter first group_by left_join mutate row_number select summarise
+#' @importFrom GenomicFeatures genes
+#' @importFrom GenomicRanges findOverlaps gaps GRanges GRangesList pintersect reduce split
+#' @importFrom ggplot2 aes coord_fixed draw_key_polygon Geom ggplot ggproto layer scale_x_continuous Stat
+#' @importFrom grid arrow gList gpar nullGrob polygonGrob segmentsGrob textGrob unit
+#' @importFrom Gviz GeneRegionTrack
+#' @importFrom InteractionSet interactions pairdist
+#' @importFrom IRanges IRanges overlapsAny
+#' @importFrom purrr map map2 map_df pmap
+#' @importFrom reticulate import_from_path
+#' @importFrom rtracklayer browserSession genome getTable ucscTableQuery
+#' @importFrom scales oob_squish
+#' @importFrom S4Vectors mcols subjectHits
+#' @importFrom tibble as_tibble is_tibble tibble
+#' @importFrom tidyr drop_na
+#' @importFrom txdbmaker makeTxDb makeTxDbFromGFF
+#' @importFrom vroom cols vroom
+#' @importFrom glue glue
 ## usethis namespace: end
 NULL
