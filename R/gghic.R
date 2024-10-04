9da3197d52421d1d0af800dac3574c08892548f2
Minghao Jiang | Office <jiangminghao1001@163.com>
2024-10-04 16:55:38 +0800

    Refactor gghic.R and gghic.Rd files, add annotation option to gghic function

--- R/gghic.R
+++ R/gghic.R
@@ -1,7 +1,3 @@
-#' @importFrom S4Vectors mcols
-#' @importFrom InteractionSet pairdist interactions
-#' @importFrom tibble as_tibble
-#' @importFrom scales oob_squish
 scale_data <- function(
     hicexperiment, score_column = "balanced", scale_method = log10
 ) {
@@ -22,28 +18,7 @@ scale_data <- function(
   x
 }
 
-#' gghic
-#'
-#' @description A ggplot2 wrapper for HiC data.
-#' @param data A HiCExperiment object or a tibble/data.frame
-#' @param score_column The column name of which the score is calculated.
-#'   Default is `"balanced"`.
-#' @param scale_method The function to scale the score. Default is `log10`.
-#' @param ideogram Whether to add ideogram or not. Default is `TRUE`.
-#' @param ... Other parameters passed to [geom_ideogram()].
-#' @return A ggplot object.
-#' @examples
-#' \dontrun{
-#' library(HiCExperiment)
-#' }
-#' @importFrom ggplot2 ggplot aes coord_fixed scale_x_continuous
-#' @importFrom tibble is_tibble
-#' @export gghic
-gghic <- function(
-  data = NULL, score_column = "balanced", scale_method = log10,
-  ideogram = TRUE,
-  ...
-) {
+ensure_data <- function(data) {
   if (is(data, "HiCExperiment")) {
     x <- scale_data(
       data, score_column = score_column, scale_method = scale_method
@@ -63,11 +38,33 @@ gghic <- function(
   } else {
     stop("data must be a HiCExperiment object or a tibble/data.frame")
   }
+  x
+}
 
-  # params <- list(...)
-  # names_param_ideogram <- c("genome", "highlight", "fontsize")
-
-  p <- x |>
+#' gghic
+#'
+#' @description A ggplot2 wrapper for HiC data.
+#' @param data A HiCExperiment object or a tibble/data.frame
+#' @param score_column The column name of which the score is calculated.
+#'   Default is `"balanced"`.
+#' @param scale_method The function to scale the score. Default is `log10`.
+#' @param ideogram Whether to add ideogram or not. Default is `TRUE`.
+#' @param annotation Whether to add annotation or not. Default is `TRUE`.
+#' @param ... Other parameters passed to [geom_ideogram()], [geom_annotation()].
+#' @return A ggplot object.
+#' @examples
+#' \dontrun{
+#' library(HiCExperiment)
+#' }
+#' @export gghic
+gghic <- function(
+  data = NULL, score_column = "balanced", scale_method = log10,
+  ideogram = TRUE, annotation = TRUE,
+  ...
+) {
+  p <- data |>
+    ensure_data() |>
+    tidyr::drop_na(score) |>
     ggplot2::ggplot(
       ggplot2::aes(
         seqnames1 = seqnames1, start1 = start1, end1 = end1,
@@ -78,12 +75,10 @@ gghic <- function(
       geom_hic() +
       theme_hic()
 
-  if (ideogram) {
-    # p <- p + do.call(
-    #   geom_ideogram, params[names(params) %in% names_param_ideogram]
-    # )
-    p <- p + geom_ideogram(...)
-  }
+  if (ideogram) p <- p + geom_ideogram(...)
+
+
+  if (annotation) p <- p + geom_annotation(...)
 
   p
 }
