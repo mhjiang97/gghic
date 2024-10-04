6fbec777845af9b83aeb39e7fd55bc20f1d052ef
Minghao Jiang | Office <jiangminghao1001@163.com>
2024-10-04 16:55:43 +0800

    Refactor utils.R, add ensure_dir and stop_if_null functions

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,13 @@
 name_pkg <- "gghic"
 
 dir_cache <- rappdirs::user_cache_dir(appname = name_pkg)
+
+ensure_dir <- function(paths) {
+  for (path in paths) {
+    if (!dir.exists(path)) dir.create(path)
+  }
+}
+
+stop_if_null <- function(object, message) {
+  if (is.null(object)) stop(message)
+}
